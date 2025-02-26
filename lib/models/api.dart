aa45b202132755775651c1a186f4ed992d1c4bb6
Wildanmusa <151824584+Wildanmusa@users.noreply.github.com>
2025-02-26 12:12:10 +0700

    Add files via upload

---
+++ lib/models/api.dart
@@ -0,0 +1,7 @@
+class BaseUrl {
+  static String tambah = "http://localhost/datawldn/create.php";
+  static String lihat = "http://localhost/datawldn/detail.php";
+  static String edit = "http://localhost/datawldn/update.php";
+  static String hapus = "http://localhost/datawldn/delete.php";
+  static String data = "http://localhost/datawldn/list.php";
+}
